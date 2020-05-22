cac2ab665d11f0268c96253efc407996b89bcd52
AdiAharonov <adi.ahronov@gmail.com>
2020-05-22 18:38:11 +0300

    sync

--- front-end/src/app.js
+++ front-end/src/app.js
@@ -8,8 +8,8 @@ import { Home } from './pages/Home'
 import { EventEdit } from './pages/EventEdit'
 import { EventDetails } from './pages/EventDetails'
 // import { UserDetails } from './pages/UserDetails'
-// import {Login} from './pages/Login'
-// import { SignUp } from './pages/SignUp'
+import {Login} from './pages/Login'
+import { SignUp } from './pages/SignUp'
 
 function App() {
 
@@ -21,10 +21,10 @@ function App() {
           <Route exact component={Home} path="/" />
           <Route exact component={EventDetails} path="/event/:eventId" />
           <Route exact component={Events} path="/event/:category?" />
+          <Route exact component={Login} path="/login" />
+          <Route exact component={SignUp} path="/signup" />
           {/* <Route exact component={EventEdit} path="/event/edit/:eventId?" /> */}
           {/* <Route exact component={UserDetails} path="/user/:userId" /> */}
-          {/* <Route exact component={SignUp} path="/signup" /> */}
-          {/* <Route exact component={Login} path="/login" /> */}
         </Switch>
       </main>
     </div>
--- front-end/src/cmps/TextField.jsx
+++ front-end/src/cmps/TextField.jsx
@@ -4,8 +4,8 @@ function TextField(props) {
     const {field, label, value, onChange, error} = props
     return (
         <div>
-            <label>{label}: </label>
-            <input type={field} name={field}  onChange={ onChange } />
+            
+            <input type={field} name={field}  onChange={ onChange } className="login-input" placeholder={label}/>
     <p className="formError">{error}</p>
         </div>
     )
