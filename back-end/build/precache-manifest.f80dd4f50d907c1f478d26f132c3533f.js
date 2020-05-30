42bbd492db31af474da6861f5723918d152ff60b
AdiAharonov <adi.ahronov@gmail.com>
2020-05-30 22:45:31 +0300

    update

---
+++ back-end/build/precache-manifest.f80dd4f50d907c1f478d26f132c3533f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29637c5d1fbfaec1fe60199e44d11b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10021cfea5da5e2a7c94",
+    "url": "/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "b94d822de41deebcb491",
+    "url": "/static/css/main.666659dc.chunk.css"
+  },
+  {
+    "revision": "10021cfea5da5e2a7c94",
+    "url": "/static/js/2.9c212752.chunk.js"
+  },
+  {
+    "revision": "2cc301c6cbe143d61f4315f038f7de47",
+    "url": "/static/js/2.9c212752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b94d822de41deebcb491",
+    "url": "/static/js/main.9b464679.chunk.js"
+  },
+  {
+    "revision": "48be1be618b5ce6a3fab",
+    "url": "/static/js/runtime-main.ba8fc901.js"
+  },
+  {
+    "revision": "afae664e1b2d250ffe1b7ffef5f5ddf5",
+    "url": "/static/media/PTSansNarrow-Regular.afae664e.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "80aba01707e5c097011c94121f1ea91b",
+    "url": "/static/media/group1.80aba017.png"
+  },
+  {
+    "revision": "a243e94f45e905103dc5e06cbb675810",
+    "url": "/static/media/group2.a243e94f.png"
+  },
+  {
+    "revision": "c5358ff61b843f24c36a784fe48c4f66",
+    "url": "/static/media/like.c5358ff6.svg"
+  },
+  {
+    "revision": "8e660389ef109b63ceb5a1e0df6ccd30",
+    "url": "/static/media/logo-stright.8e660389.png"
+  },
+  {
+    "revision": "98e9811226de7e2e33820e30a44effe2",
+    "url": "/static/media/main-bg.98e98112.jpg"
+  }
+]);
